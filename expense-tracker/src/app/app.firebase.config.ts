760bcafd4da2e3bf930cf2d605c24e7df2df7a37
Jai SP <jaisprabhu1@gmail.com>
2024-09-25 18:02:16 +0530

    new

---
+++ expense-tracker/src/app/app.firebase.config.ts
@@ -0,0 +1,20 @@
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyAdENYvQg1gI_1pONInILcbQNX_Ji4Bss8',
+//   authDomain: 'expense-tracker-e0028.firebaseapp.com',
+//   databaseURL: 'https://expense-tracker-e0028.firebaseio.com',
+//   projectId: 'expense-tracker-e0028',
+//   storageBucket: 'expense-tracker-e0028.appspot.com',
+//   messagingSenderId: '927113629451'
+// };
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSgXulgoP5YJVdgRofFKBIePB6Twn3sHI",
+  authDomain: "expense-tracker-d5051.firebaseapp.com",
+  databaseURL:'https://expense-tracker-d5051-default-rtdb.firebaseio.com/',
+  projectId: "expense-tracker-d5051",
+  storageBucket: "expense-tracker-d5051.appspot.com",
+  messagingSenderId: "54681524926",
+  appId: "1:54681524926:web:f13917ce38944d62fb5cef",
+  measurementId: "G-4MYWMT5EX0"
+}
